5dc4a576eeb09466943f312afe2cc3483b621e05
zuberbrian <37417090+zuberbrian@users.noreply.github.com>
2018-04-30 23:30:38 -0700

    Add files via upload

---
+++ for_loop_basic2.py
@@ -0,0 +1,45 @@
+# Biggie Size
+# def biggie(arr):
+#     for number in range(0,len(arr)):
+#         if arr[number] > 0:
+#             arr[number]="Big"
+#     print(arr)
+# biggie([2,-7,5,3,-2,-6])
+# Count Positives
+# def positive(arr):
+#     number = 0
+#     for i in range(0,len(arr)):
+#         if arr[i] > 0:
+#             number += 1
+#     arr[len(arr)-1] = number
+#     print(arr)
+# positive([-1,5,-4,7,-8,9])
+#SumTotal
+# def sumtotal(arr):
+#     sum = arr[0]
+#     for i in range(1,len(arr)):
+#         sum = sum + arr[i]
+#     print(sum)
+#     return arr
+# sumtotal([1,2,3,4,5])
+#Average
+# def average(arr):
+#     max = arr[0]
+#     min = arr[0]
+#     sum = arr[0]
+#     avg = 0
+#     for i in range(1, len(arr)):
+#         if arr[i] > max:
+#             max = arr[i]
+#         if arr[i] < min:
+#             min=arr[i]
+#         sum = sum + arr[i]
+#     avg = sum/len(arr)
+#     print(avg)
+# average([1,2,3,4])
+#Length
+# def length(arr)
+#     for i in range(0,len.arr):
+#     print(len(arr))
+# length([1,2,3,4])
+##What##
